fc9ec72a04d62f9f84b9b54b2a71e2da0fb5be5d
Alexander Palatov <alexpav801@gmail.com>
2021-08-15 17:46:53 +0300

    re init client with vue 3

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,6 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
-})
+createApp(App).use(store).use(router).mount('#app');
---
+++ client/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="title">FileX is up n running!</h1>
+</template>
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+<style lang="scss" scoped>
+.title {
+  color: green;
+}
+</style>
